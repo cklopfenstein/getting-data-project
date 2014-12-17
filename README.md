710ae8dc1da889b2bd5c185f1f6bee0f7c83f69d
Chris Klopfenstein <klopfenstein.chris@gmail.com>
2014-12-17 00:25:09 -0800

    add some files

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ##README.md
 
 getting-data-project repository  - an assignment for the Coursera "Getting and Cleaning Data" course.
+
+###Contents
+
+1. **run_analysis.R** - an R script to analyze Samsung data (Human Activity Recognition Using Smartphones)
+
+2. **CodeBook.md** - a description of the variables in the tidy data produced by run_analysis.R
+
+3. **UCIdocs** - a folder containing documentation about the experiment, and the raw data, from the UCI Human Activity Recognition study.
---
+++ run_analysis.R
@@ -0,0 +1,10 @@
+# run_analysis.R
+# Process raw data from UCI HAR into tidy dataset
+setwd("~/Devel/R/DataScience/GettingData/Project/GettingDataProject")  # need to change this eventually
+# read the input files
+X_test <- read.csv("../UCI HAR Dataset/test/X_test.txt", sep = " ")
+y_test <- read.csv("../UCI HAR Dataset/test/y_test.txt", sep = " ")
+subject_test <- read.csv("../UCI HAR Dataset/test/subject_test.txt", sep = " ")
+X_train <- read.csv("../UCI HAR Dataset/train/X_train.txt", sep = " ")
+y_train <- read.csv("../UCI HAR Dataset/train/y_train.txt", sep = " ")
+subject_train <- read.csv("../UCI HAR Dataset/train/subject_train.txt", sep = " ")
